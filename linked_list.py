093e405eb5a11b7c77dc84d361ad21f875e562ae
Michael Neely <michael@michaelneely.net>
2023-09-07 11:35:03 -0400

    Added Linked LIst section

---
+++ linked_list.py
@@ -0,0 +1,12 @@
+class Node:
+    """An object for string a single node for a lined list.
+        Models two attributes - data and the link to the next node in the list
+    """
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" %self.data
