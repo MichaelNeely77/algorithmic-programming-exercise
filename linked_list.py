fbd6ef1567ff85c8e2141905215d2356aa3fd72d
Michael Neely <michael@michaelneely.net>
2023-09-07 11:53:14 -0400

    Added a while loop and getting ready to see what is next

--- linked_list.py
+++ linked_list.py
@@ -10,3 +10,26 @@ class Node:
 
     def __repr__(self):
         return "<Node data: %s>" %self.data
+class LinkedList:
+    """
+    Singly linked list
+    """
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+    
+    def size(self):
+        """
+        Returns the number of nodes in the list
+        Take 0(n) time - linear time
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current - current.next_node
+
+        return count
