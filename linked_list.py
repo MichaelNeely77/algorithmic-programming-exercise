d60c35178d54cb2a008dee92547de15e614b823a
Michael Neely <michael@michaelneely.net>
2023-09-30 16:39:34 -0400

    Added the __repr__ function to display the Head and Tail Nodes of a LinkedList

--- linked_list.py
+++ linked_list.py
@@ -35,3 +35,31 @@ class LinkedList:
         return count
     
     # Ends at 2:36:29
+
+    def add(self, data):
+        """
+        Adds new Node containing data at head of the list
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def __repr__(self):
+        """
+        Return a string representation of the list
+        Takes O(n) time - linear time
+        """
+
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+
+            current = current.next_node
+        return '-> '.join(nodes)
